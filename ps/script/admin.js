879b50ca74f966bde148c7d210a2d36d4c082ed6
Medovaya Lojka <Yulia41898@mail.ru>
2021-11-21 20:52:08 +0300

    Добавлена отправка на странице администратора, внесены изменения в верстке, сделано добавление картинок

--- ps/script/admin.js
+++ ps/script/admin.js
@@ -1,3 +1,75 @@
-window.onload(()=>{
+let buttonCounter = 0;
+let currentModalId;
+const imageList = [];
+const MAX_IMAGE_COUNT = 2;
+
+const closeModal = () => {
+    document.getElementById("modalContainer").style.display = "none";
+}
+
+const openModal = (e) => {
+    document.getElementById("modalContainer").style.display = "flex";
+    console.log(e);
+    currentModalId = e.id;
+}
+
+const copyAddButton = () => {
     
+    if (buttonCounter < MAX_IMAGE_COUNT) {
+        const imageButton = document.getElementById("addButton").cloneNode(true);
+        imageButton.id = "addButton" + buttonCounter;
+        imageButton.style.display = "block";
+        document.getElementById("imageContainer").appendChild(imageButton);
+        buttonCounter++;
+    }   
+}
+
+const modalConfirm = () => {
+    closeModal();
+    const picURL = document.getElementById("picURL").value;
+    const addCur = document.getElementById(currentModalId);
+    if(picURL) {
+        addCur.innerHTML = "";
+        addCur.style.backgroundImage = `url(${picURL})`;
+        imageList[buttonCounter-1] = picURL;
+        copyAddButton();
+    }
+    document.getElementById("picURL").value = "";
+}
+
+window.addEventListener("load", () => {
+    copyAddButton();
+    document.getElementById("modal").addEventListener("mousedown", (e) => {
+        e.stopPropagation();
+    })
 })
+
+const sendData = () => {
+    const positionData = {
+        name: document.getElementById("name").value,
+        section: document.getElementById("section").value,
+        category: document.getElementById("category").value,
+        description: document.getElementById("description").value,
+        imageList: imageList,
+        adminPass: document.getElementById("adminPass").value
+    }
+    set('/setPosition', positionData).then((data) => {
+        fetch('/getPositionsList')
+            .then(response => response.json())
+            .then(data => console.log(data));
+    });
+} 
+
+async function set(url, params) {
+    const rawResponse = await fetch(url, {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(params)
+    });
+
+    return await rawResponse.json();
+}
+
